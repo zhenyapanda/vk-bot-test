964109a238e7ddb29ba40fdada9887981e663dd0
hypnoShark <xpurex@yandex.ru>
2018-05-14 13:45:48 +0300

    to war

--- src/main/java/project/ChatBotApplication.java
+++ src/main/java/project/ChatBotApplication.java
@@ -2,9 +2,16 @@ package project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChatBotApplication {
+public class ChatBotApplication extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ChatBotApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ChatBotApplication.class, args);
